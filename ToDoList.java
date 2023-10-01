d7079a761f8bfee1e7f2234e7f8bd2f74f97a9d4
sanamehdi7 <144696500+sanamehdi7@users.noreply.github.com>
2023-10-01 00:10:45 -0400

    Update ToDoList.java

--- ToDoList.java
+++ ToDoList.java
@@ -1,42 +1,30 @@
 package api;
-
-import entity.Grade;
 import entity.Projects;
 import okhttp3.*;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.Arrays;
-
 
 public class ToDoList implements Todo{
-
-    private static final String API_URL = "@doist/todoist-api-typescript";
 
     @Override
     public Projects getProject(String name) {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url(String.format("https://grade-logging-api.chenpan.ca/grade?course=%s&utorid=%s", name))
-                .addHeader("Content-Type", "application/json")
+                .url(String.format("@doist/todoist-api-typescript", name))
                 .build();
         try {
             Response response = client.newCall(request).execute();
             System.out.println(response);
             JSONObject responseBody = new JSONObject(response.body().string());
 
-            if (responseBody.getInt("status_code") == 200) {
-                JSONObject grade = responseBody.getJSONObject("grade");
-                return Projects.builder()
-                        .name(Projects.getName("name"))
-                        .favourite(grade.getString("course"))
-                        .build();
-            } else {
-                throw new RuntimeException(responseBody.getString("message"));
-            }
+            JSONObject project = responseBody.getJSONObject("project");
+            return Projects.builder()
+                    .name(Projects.getName())
+                    .favourite(Projects.getFavourite())
+                    .build();
+
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
