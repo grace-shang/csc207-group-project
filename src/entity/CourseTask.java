43bd6f1e35c4b98e9889eb992155ad93e83cd1ba
graceshang <grace.ry.shang@gmail.com>
2023-11-12 12:05:26 -0500

    added overloaded and default constructor

--- src/entity/CourseTask.java
+++ src/entity/CourseTask.java
@@ -4,6 +4,13 @@ public class CourseTask extends Task{
 
     private Course course;
 
+    // Default constructor
+    public CourseTask(String taskName, Course course) {
+        super(taskName);
+        this.course = course;
+    }
+
+    // Overloaded constructor
     public CourseTask(String taskName, boolean complete, Course course) {
         super(taskName, complete);
         this.course = course;
