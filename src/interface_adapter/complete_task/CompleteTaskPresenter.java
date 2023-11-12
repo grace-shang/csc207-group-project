2c97a562b217b4c7040d6d4dd0dbc2edd9a44743
graceshang <grace.ry.shang@gmail.com>
2023-11-11 22:44:33 -0500

    added presenter methods

--- src/interface_adapter/complete_task/CompleteTaskPresenter.java
+++ src/interface_adapter/complete_task/CompleteTaskPresenter.java
@@ -1,4 +1,20 @@
-package src.interface_adapter.complete_task;
+package interface_adapter.complete_task;
 
-public class CompleteTaskPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.complete_task.CompleteTaskOutputBoundary;
+import use_case.create_task.CreateTaskOutputBoundary;
+
+public class CompleteTaskPresenter implements CompleteTaskOutputBoundary {
+    private final CompleteTaskViewModel completeTaskViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public CompleteTaskPresenter(ViewManagerModel viewManagerModel, CompleteTaskViewModel completeTaskViewModel) {
+        this.completeTaskViewModel = completeTaskViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(CreateTaskOutputBoundary task) {
+
+    }
 }
