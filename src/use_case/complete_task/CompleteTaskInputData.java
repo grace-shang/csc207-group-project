2a69d3eadf43c8d705ffb99d5c48d39b3f4c0c03
graceshang <grace.ry.shang@gmail.com>
2023-11-11 22:17:58 -0500

    added getter

--- src/use_case/complete_task/CompleteTaskInputData.java
+++ src/use_case/complete_task/CompleteTaskInputData.java
@@ -1,4 +1,12 @@
 package use_case.complete_task;
 
 public class CompleteTaskInputData {
+    private final String taskName;
+    public CompleteTaskInputData(String task) {
+        this.taskName = task;
+    }
+
+    public String getTaskName(){
+        return taskName;
+    }
 }
