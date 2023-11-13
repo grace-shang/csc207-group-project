382ebbca026326a405b5d6ac7ba7db6c18b4a307
graceshang <grace.ry.shang@gmail.com>
2023-11-13 10:49:59 -0500

    wrote constructor and getter

--- src/entity/Task.java
+++ src/entity/Task.java
@@ -6,7 +6,7 @@ class Task implements TaskI{
     boolean complete;
 
     // Default constructor. Default value of complete is false.
-    public Task(String taskName, String projectName) {
+    public Task(String taskName) {
         this.taskName = taskName;
         this.complete = false;
     }
--- src/interface_adapter/complete_task/CompleteTaskPresenter.java
+++ src/interface_adapter/complete_task/CompleteTaskPresenter.java
@@ -2,7 +2,9 @@ package interface_adapter.complete_task;
 
 import interface_adapter.ViewManagerModel;
 import use_case.complete_task.CompleteTaskOutputBoundary;
+import use_case.complete_task.CompleteTaskOutputData;
 import use_case.create_task.CreateTaskOutputBoundary;
+import use_case.create_task.CreateTaskOutputData;
 
 public class CompleteTaskPresenter implements CompleteTaskOutputBoundary {
     private final CompleteTaskViewModel completeTaskViewModel;
@@ -14,7 +16,7 @@ public class CompleteTaskPresenter implements CompleteTaskOutputBoundary {
     }
 
     @Override
-    public void prepareSuccessView(CreateTaskOutputBoundary task) {
+    public void prepareSuccessView(CompleteTaskOutputData completed) {
 
     }
 }
