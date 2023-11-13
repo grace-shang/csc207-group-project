4b5d501dde1d01398f072251817c07195b4f8f61
graceshang <grace.ry.shang@gmail.com>
2023-11-12 19:06:24 -0500

    wrote class constructor and variables

--- src/use_case/complete_task/CompleteTaskOutputData.java
+++ src/use_case/complete_task/CompleteTaskOutputData.java
@@ -1,4 +1,9 @@
 package use_case.complete_task;
 
 public class CompleteTaskOutputData {
+    private boolean completeTaskFailed;
+
+    public CompleteTaskOutputData(boolean completeTaskFailed){
+        this.completeTaskFailed = completeTaskFailed;
+    }
 }
