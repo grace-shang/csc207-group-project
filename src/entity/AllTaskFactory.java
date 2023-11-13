9da34f64453c299ddaa90d77164e3b0be6a0c2db
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-11-13 09:58:54 -0500

    Creating an AllTaskFactory to create the tasks given a task name.

--- src/entity/AllTaskFactory.java
+++ src/entity/AllTaskFactory.java
@@ -1,2 +1,11 @@
-package entity;public class AllTaskFactory {
+package entity;
+
+import java.time.LocalDateTime;
+
+public class AllTaskFactory implements TaskFactory{
+    @Override
+    public Task create(String taskName) {
+        return new Task(taskName);
+    }
+
 }
