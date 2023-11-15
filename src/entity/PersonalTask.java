99b5fec545830c5fd6f6824e57a3b483b41ae5e0
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-11-15 00:46:25 -0500

    Created a project name instance attribute and added it to the constructors.

--- src/entity/PersonalTask.java
+++ src/entity/PersonalTask.java
@@ -4,14 +4,18 @@ public class PersonalTask extends Task{
 
     private String event;
     private String location;
+    private String projectName;
 
     public PersonalTask(String taskName, boolean complete, String event, String location) {
         super(taskName, complete);
         this.event = event;
         this.location = location;
+        this.projectName = "Personal";
     }
 
     public String getEvent() {return event;}
 
     public String getLocation() {return location;}
+
+    public String getProjectName() {return projectName;}
 }
