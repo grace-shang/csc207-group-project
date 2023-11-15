bce87cbf9c379bfbf470c362c13030d74d025a8f
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-11-14 20:13:23 -0500

    Creating the data access object that will keep the information in memory.

--- src/data_access/InMemoryTaskDataAccessObject.java
+++ src/data_access/InMemoryTaskDataAccessObject.java
@@ -1,2 +1,39 @@
-package data_access;public class InMemoryTaskDataAccessObject {
+package data_access;
+
+import api.Todo;
+import entity.TaskI;
+import use_case.complete_task.CompleteTaskDataAccessInterface;
+import use_case.create_task.CreateTaskDataAccessInterface;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryTaskDataAccessObject implements CompleteTaskDataAccessInterface, CreateTaskDataAccessInterface{
+
+    private final Map<String, TaskI> tasks = new HashMap<>();
+    private Todo todo;
+
+    public InMemoryTaskDataAccessObject(Todo todo){
+        this.todo = todo;
+    }
+
+    /**
+     * @param task the data to save
+     */
+    public void save(TaskI task) {
+        tasks.put(task.getName(), task);
+    }
+
+    @Override
+    public void addTask(TaskI taskI){
+//        String projectName = taskI.getProjectName();
+        String taskName = taskI.getName();
+        todo.addTask("projectName", taskName);
+    }
+
+    @Override
+    public void complete(TaskI task) throws IOException {
+
+    }
 }
