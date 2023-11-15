04c0b72862fe8ec9855de1bc8312151dca3c93d2
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-11-15 00:45:51 -0500

    Created a project name instance variable and implemented it within the constructors in Course Task.

--- src/entity/CourseTask.java
+++ src/entity/CourseTask.java
@@ -3,17 +3,20 @@ package entity;
 public class CourseTask extends Task{
 
     private Course course;
+    private String projectName;
 
     // Default constructor
     public CourseTask(String taskName, Course course) {
         super(taskName);
         this.course = course;
+        this.projectName = "Courses";
     }
 
     // Overloaded constructor
     public CourseTask(String taskName, boolean complete, Course course) {
         super(taskName, complete);
         this.course = course;
+        this.projectName = "Courses";
     }
 
     public Course getCourse() {
