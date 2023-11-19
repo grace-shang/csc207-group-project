9a4a5a4254fecdde23a7fa1c9bb40bac5a3742e4
graceshang <grace.ry.shang@gmail.com>
2023-11-18 21:02:26 -0500

    modified constructor and added a task parameter

--- src/use_case/complete_task/CompleteTaskOutputData.java
+++ src/use_case/complete_task/CompleteTaskOutputData.java
@@ -2,8 +2,10 @@ package use_case.complete_task;
 
 public class CompleteTaskOutputData {
     private boolean completeTaskFailed;
+    private final String task;
 
-    public CompleteTaskOutputData(boolean completeTaskFailed){
+    public CompleteTaskOutputData(boolean completeTaskFailed, String task){
         this.completeTaskFailed = completeTaskFailed;
+        this.task = task;
     }
 }
