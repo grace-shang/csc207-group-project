33cb7fa06dd1c1110e04b12e147d586fea505bc8
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-11-20 10:49:31 -0500

    created the state for the display task use case.

--- src/interface_adapter/display_task/DisplayTaskState.java
+++ src/interface_adapter/display_task/DisplayTaskState.java
@@ -1,2 +1,30 @@
-package interface_adapter.display_task;public class DisplayTaskState {
+package interface_adapter.display_task;
+
+import java.util.Set;
+
+public class DisplayTaskState {
+
+    private Set<String> tasks = null;
+    private String taskNames = null;
+
+    public DisplayTaskState(DisplayTaskState copy) {
+        tasks = copy.tasks;
+        taskNames = copy.taskNames;
+    }
+
+    public DisplayTaskState() {
+    }
+
+    public Set<String> getTasks() {return tasks;}
+
+    public void setTasks(Set<String> tasks) {this.tasks = tasks;}
+
+    public String getTaskNames() {
+        return taskNames;
+    }
+
+    public static void setTaskNames(String taskNames) {
+        this.taskNames = taskNames;
+    }
+
 }
