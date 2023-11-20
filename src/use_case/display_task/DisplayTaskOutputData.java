06b1929d7df83d84302a19b86d83c7c8b736a289
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-11-20 10:54:47 -0500

    Created the output data for the display use case.

--- src/use_case/display_task/DisplayTaskOutputData.java
+++ src/use_case/display_task/DisplayTaskOutputData.java
@@ -1,2 +1,29 @@
-package use_case.display_task;public class DisplayTaskOutputData {
+package use_case.display_task;
+
+import java.util.Set;
+
+public class DisplayTaskOutputData {
+
+    private final Set<String> tasks;
+    private String taskNames;
+
+    private boolean useCaseFailed;
+
+    public DisplayTaskOutputData(Set<String> tasks, String taskNames, boolean useCaseFailed) {
+        this.tasks = tasks;
+        this.taskNames = taskNames;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public Set<String> getAllTasks() {return tasks;}
+
+    public String getTaskNames() {
+        return taskNames;
+    }
+
+    public void setTaskNames(String taskNames) {
+        this.taskNames = taskNames;
+    }
+
+
 }
