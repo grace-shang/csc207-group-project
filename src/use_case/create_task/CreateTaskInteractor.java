043d9f542f6e1c1734f035c53bad626db561a599
sashachugani22 <sasha.chugani@mail.utoronto.ca>
2023-11-27 09:38:29 -0500

    Changes to interactor

--- src/use_case/create_task/CreateTaskInteractor.java
+++ src/use_case/create_task/CreateTaskInteractor.java
@@ -1,3 +1,32 @@
 package use_case.create_task;
 
+import entity.TaskI;
+import entity.TaskFactory;
 
+public class CreateTaskInteractor implements CreateTaskInputBoundary{
+    final CreateTaskDataAccessInterface taskDataAccessObject;
+
+    final CreateTaskOutputBoundary userPresenter;
+
+    final TaskFactory taskFactory;
+
+    public CreateTaskInteractor(CreateTaskDataAccessInterface taskDataAccessObject,
+                                CreateTaskOutputBoundary userPresenter,
+                                TaskFactory taskFactory){
+        this.taskDataAccessObject = taskDataAccessObject;
+        this.userPresenter = userPresenter;
+        this.taskFactory = taskFactory;
+    }
+
+    public void execute(CreateTaskInputData createTaskInputData){
+        TaskI taskI = taskFactory.create(createTaskInputData.getCreateTask(), false);
+        taskDataAccessObject.save(taskI);
+
+        taskDataAccessObject.addTask(taskI);
+
+        CreateTaskOutputData createTaskOutputData = new CreateTaskOutputData(taskI.getName());
+    }
+
+
+
+}
