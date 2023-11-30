f1b74aa0cbf6a0f70b076ed51d84dfb255643054
graceshang <grace.ry.shang@gmail.com>
2023-11-30 13:35:04 -0500

    added setter for completion status

--- src/interface_adapter/complete_task/CompleteTaskState.java
+++ src/interface_adapter/complete_task/CompleteTaskState.java
@@ -1,5 +1,6 @@
 package interface_adapter.complete_task;
 
+import entity.AllTaskFactory;
 import entity.TaskI;
 import interface_adapter.create_task.CreateTaskState;
 
@@ -13,4 +14,10 @@ public class CompleteTaskState {
     public String getTaskName() {
         return task;
     }
+
+    public void setTaskCompletion(TaskI task){
+        task.setComplete(true);
+    }
+
+
 }
