4a8e4c0a8839a65a693cac242a0a3f201c2790ea
sashachugani22 <sasha.chugani@mail.utoronto.ca>
2023-12-01 12:00:54 -0500

    Setting up test cases, still have to fill in code and test the code.

--- test/create_task/CreateTaskViewTest.java
+++ test/create_task/CreateTaskViewTest.java
@@ -38,150 +38,31 @@ public class CreateTaskViewTest {
         ftdao.save(tf.create("task2", false));
     }
 
-    // END OF FIXED TEST CODE
-
-    public JButton getButton() {
-        JFrame app = null;
-        Window[] windows = Window.getWindows();
-        for (Window window : windows) {
-            if (window instanceof JFrame) {
-                app = (JFrame) window;
-            }
-        }
-
-
-        assertNotNull(app); // found the window?
-
-        Component root = app.getComponent(0);
-
-        Component cp = ((JRootPane) root).getContentPane();
-
-        JPanel jp = (JPanel) cp;
-
-        JPanel jp2 = (JPanel) jp.getComponent(0);
-
-        TaskView tv = (TaskView) jp2.getComponent(0);
-
-        JPanel buttons = (JPanel) tv.getComponent(2);
-
-        return (JButton) buttons.getComponent(1); // this should be the clear button
+    public JButton getButton(){
+        JButton placeholder = new JButton();
+        return placeholder;
     }
 
-    /**
-     *
-     * Test that the Create button is present and where it is expected to be
-     */
-    @org.junit.Test
-    public void testCreateButtonPresent() {
-        Main.main(null);
-        JButton createTaskButton = getButton();
-        assert(createTaskButton.getText().equals("Create Task"));
-    }
 
-    /**
-     *
-     * Test that pressing the Clear button deletes all users. This test first
-     * adds two users to the CSV file, then starts the program, then clicks the Clear button,
-     * and then checks that the file's length has decreased.
-     */
     @org.junit.Test
-    public void testCreateAddTasksToFile() {
-
-        addTwoTasks();
+    public void testCompleteButtonPresent(){
         Main.main(null);
         JButton button = getButton();
-
-        // since clicking the button should end up displaying a JDialog to the user to report the
-        // result, we set a timer, which will execute code necessary to complete the testing.
-        createCloseTimer().start();
-
-        button.doClick();
-
-        // will continue execution here after the JDialog is closed
-
-        // users.csv format
-        //username, password,timestamp (in format returned by a call like LocalDateTime.now())
-        //example
-        //user1,pass1,2023-10-12T14:46:26.733518
-
-        //check the users.csv file to ensure the users are gone
-        try {
-            int lines = countLines();
-            System.out.println("lines left = " + lines);
-            assert(lines == 2);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        assert(button.getText().equals("Create Task"));
     }
 
-    /**
-     *
-     * This test is the same as above, but it additionally checks that the JDialog contains the names of
-     * all users deleted from the file.
-     */
 
     @org.junit.Test
-    public void testCreateTaskAddsTaskToMainPage() throws InterruptedException {
+    public void testCreateSavesTaskIntoFile(){}
 
-        addTwoTasks();
-        message = "";
+    @org.junit.Test
+    public void testCreateDisplaysNewTask(){}
 
-        Main.main(null);
 
-        JButton button = getButton();
+    @org.junit.Test
+    public void testLinesInCSVAfterCreate(){}
 
-        // since clicking the button should end up displaying a JDialog to the user to report the
-        // result, we set a timer, which will execute code necessary to complete the testing.
-        createCloseTimer().start();
 
-        //click the button
-        button.doClick();
-
-        // will continue execution here after the JDialog is closed
-
-        // check the message
-        assert(message.contains("user1") && message.contains("user2"));
-    }
-
-    private Timer createCloseTimer() {
-        ActionListener close = new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                Window[] windows = Window.getWindows();
-                for (Window window : windows) {
-
-                    if (window instanceof JDialog) {
-
-                        JDialog dialog = (JDialog)window;
-
-                        // this ignores old dialogs
-                        if (dialog.isVisible()) {
-                            String s = ((JOptionPane) ((BorderLayout) dialog.getRootPane()
-                                    .getContentPane().getLayout()).getLayoutComponent(BorderLayout.CENTER)).getMessage().toString();
-                            System.out.println("message = " + s);
-
-                            // store the information we got from the JDialog
-//                            ClearUsersTest.message = s;
-//                            ClearUsersTest.popUpDiscovered = true;
-
-                            System.out.println("disposing of..." + window.getClass());
-                            window.dispose();
-                        }
-                    }
-                }
-            }
-
-        };
-
-        Timer t = new Timer(1000, close);
-        t.setRepeats(false);
-        return t;
-    }
 
     private static int countLines() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("tasks.csv"));
