ba092ac82895af3f902ab09a41a1ef235a25043d
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-01 10:55:57 -0500

    Creating the display task interactor. Created the execute method (still in progess, errors in displayTaskDataAccessObject and presenter.

--- src/use_case/display_task/DisplayTaskInteractor.java
+++ src/use_case/display_task/DisplayTaskInteractor.java
@@ -1,4 +1,25 @@
 package use_case.display_task;
 
 public class DisplayTaskInteractor {
+
+    final DisplayTaskDataAccessInterface displayDataAccessObject;
+    final DisplayTaskOutputBoundary taskPresenter;
+
+    /**
+     *
+     * @param displayDataAccessObject
+     * @param taskPresenter
+     */
+    public DisplayTaskInteractor(DisplayTaskDataAccessInterface displayDataAccessObject, DisplayTaskOutputBoundary taskPresenter) {
+        this.displayDataAccessObject = displayDataAccessObject;
+        this.taskPresenter = taskPresenter;
+    }
+
+    public void execute() {
+        DisplayTaskOutputData displayTaskOutputData = new DisplayTaskOutputData(displayTaskDataAccessObject.getAllTasks().keySet(), false, String.join(",", userDataAccessObject.getAllUsers().keySet()));
+        displayTaskDataAccessObject.display(displayTaskDataAccessObject.getAllTasks());
+        displayTaskPresenter.prepareSuccessView(displayTaskOutputData);
+
+    }
+
 }
