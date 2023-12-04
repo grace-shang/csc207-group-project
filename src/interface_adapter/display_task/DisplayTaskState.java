17023d071307544f8352be57733502df9f0c8693
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-04 15:48:22 -0500

    Created the JavaDoc for the display state public methods.

--- src/interface_adapter/display_task/DisplayTaskState.java
+++ src/interface_adapter/display_task/DisplayTaskState.java
@@ -8,22 +8,45 @@ public class DisplayTaskState {
 
     private ArrayList<ArrayList<Object>> taskInfo = new ArrayList<>();
 
+    /**
+     * Constructs the display task state
+     * @param copy the state of the display use case
+     */
     public DisplayTaskState(DisplayTaskState copy) {
         this.tasks = copy.tasks;
         this.taskInfo = copy.taskInfo;
     }
 
+    /**
+     * Constructor for the display task state with no given state
+     */
     public DisplayTaskState() {
     }
 
+    /**
+     * Getter for the list of task names
+     * @return list of task names
+     */
     public ArrayList<String> getTasks() {return tasks;}
 
+    /**
+     * Setter for the task names
+     * @param tasks A list of task names
+     */
     public void setTasks(ArrayList<String> tasks) {this.tasks = tasks;}
 
+    /**
+     * Getter for the task information
+     * @return a list that contains lists of objects with task information
+     */
     public ArrayList<ArrayList<Object>> getTaskInfo() {
         return taskInfo;
     }
 
+    /**
+     * Setter for the task information
+     * @param taskInfo a list that contains lists of objects with task information
+     */
     public void setTaskInfo(ArrayList<ArrayList<Object>> taskInfo) {
         this.taskInfo = taskInfo;
     }
