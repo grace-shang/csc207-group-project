b0a73e4a96ab699afb066a694831eb9d87397aaa
sashachugani22 <sasha.chugani@mail.utoronto.ca>
2023-12-04 16:37:17 -0500

    Got the coverage working.

--- test/create_task/CreateTaskViewTest.java
+++ test/create_task/CreateTaskViewTest.java
@@ -33,24 +33,52 @@ public class CreateTaskViewTest {
      * ensures there are at least 2 tasks in the CSV file for testing purposes
      */
 
-    //@Test
-    //    public void successTest() {
-    //        CreateTaskInputData inputData = new CreateTaskInputData("task1");
-    //        CreateTaskDataAccessInterface taskRepository = new InMemoryTaskDataAccessObject(todo);
-    //
-    //        // This creates a successPresenter that tests whether the test case is as we expect.
-    //        CreateTaskOutputBoundary successPresenter = new CreateTaskOutputBoundary() {
-    //            @Override
-    //            public void prepareSuccessView(CreateTaskOutputData task) {
-    //                // 2 things to check: the output data is correct, and the user has been created in the DAO.
-    //                assertEquals("task1", task.getTask());
-    //                assertTrue(taskRepository.existByName("task1"));
-    //            }
-    //        };
-    //        CreateTaskInputBoundary interactor = new CreateTaskInteractor(taskRepository, successPresenter, new AllTaskFactory());
-    //        interactor.execute(inputData);
-    //    }
+    @Test
+    public void successTest() {
+        CreateTaskInputData inputData = new CreateTaskInputData("task1");
+        CreateTaskDataAccessInterface taskRepository = new InMemoryTaskDataAccessObject(todo);
 
+        // This creates a successPresenter that tests whether the test case is as we expect.
+        CreateTaskOutputBoundary successPresenter = new CreateTaskOutputBoundary() {
+            @Override
+            public void prepareSuccessView(CreateTaskOutputData task) {
+                // 2 things to check: the output data is correct, and the user has been created in the DAO.
+                assertEquals("task1", task.getTask());
+                assertTrue(taskRepository.existByName("task1"));
+            }
+
+            @Override
+            public void prepareFailView(String error){
+                fail("Use case failure is unexpected.");
+            }
+        };
+
+        CreateTaskInputBoundary interactor = new CreateTaskInteractor(taskRepository, successPresenter, new AllTaskFactory());
+        interactor.execute(inputData);
+    }
+
+
+    @Test
+    public void failTest() {
+        CreateTaskInputData inputData = new CreateTaskInputData("");
+        CreateTaskDataAccessInterface taskRepository = new InMemoryTaskDataAccessObject(todo);
+
+        // This creates a successPresenter that tests whether the test case is as we expect.
+        CreateTaskOutputBoundary failPresenter = new CreateTaskOutputBoundary() {
+            @Override
+            public void prepareSuccessView(CreateTaskOutputData task) {
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error){
+                assertEquals("An Empty Task Can't Be Added", error);
+            }
+        };
+
+        CreateTaskInputBoundary interactor = new CreateTaskInteractor(taskRepository, failPresenter, new AllTaskFactory());
+        interactor.execute(inputData);
+    }
 
 
 
