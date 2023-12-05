edb980bbb077792a5e8e81a73c789e3178907c52
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-04 20:42:19 -0500

    Commenting all code that is not directly used within the program for testing purposes.

--- src/api/ToDoList.java
+++ src/api/ToDoList.java
@@ -12,33 +12,33 @@ import java.net.URL;
 import java.io.IOException;
 
 public class ToDoList implements Todo{
-    @Override
-
-    public Projects getProject(String name) {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        Request request = new Request.Builder()
-                .url(String.format("@doist/todoist-api-typescript", name)).build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            System.out.println(response);
-            JSONObject responseBody = new JSONObject(response.body().string());
-
-            JSONObject project = responseBody.getJSONObject("project");
-            return Projects.builder()
-                    .name(Projects.getName())
-                    .favourite(Projects.getFavourite())
-                    .build();
-
-        } catch (IOException | JSONException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public Projects logProject(String name, boolean favourite) throws JSONException {
-        return null;
-    }
+//    @Override
+//
+//    public Projects getProject(String name) {
+//        OkHttpClient client = new OkHttpClient().newBuilder().build();
+//        Request request = new Request.Builder()
+//                .url(String.format("@doist/todoist-api-typescript", name)).build();
+//
+//        try {
+//            Response response = client.newCall(request).execute();
+//            System.out.println(response);
+//            JSONObject responseBody = new JSONObject(response.body().string());
+//
+//            JSONObject project = responseBody.getJSONObject("project");
+//            return Projects.builder()
+//                    .name(Projects.getName())
+//                    .favourite(Projects.getFavourite())
+//                    .build();
+//
+//        } catch (IOException | JSONException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @Override
+//    public Projects logProject(String name, boolean favourite) throws JSONException {
+//        return null;
+//    }
 
 //    @Override
 //    public void deleteTask(String projectName, String taskName) {
--- src/entity/CourseTask.java
+++ src/entity/CourseTask.java
@@ -1,25 +1,25 @@
-package entity;
-
-public class CourseTask extends Task{
-
-    private final Course course;
-    private final String projectName;
-
-    // Default constructor
-    public CourseTask(String taskName, Course course) {
-        super(taskName);
-        this.course = course;
-        this.projectName = "Courses";
-    }
-
-    // Overloaded constructor
-    public CourseTask(String taskName, boolean complete, Course course) {
-        super(taskName, complete);
-        this.course = course;
-        this.projectName = "Courses";
-    }
-
-    public Course getCourse() {
-        return course;
-    }
-}
+//package entity;
+//
+//public class CourseTask extends Task{
+//
+//    private final Course course;
+//    private final String projectName;
+//
+//    // Default constructor
+//    public CourseTask(String taskName, Course course) {
+//        super(taskName);
+//        this.course = course;
+//        this.projectName = "Courses";
+//    }
+//
+//    // Overloaded constructor
+//    public CourseTask(String taskName, boolean complete, Course course) {
+//        super(taskName, complete);
+//        this.course = course;
+//        this.projectName = "Courses";
+//    }
+//
+//    public Course getCourse() {
+//        return course;
+//    }
+//}
