64e4fc4525e1feb3d685fb85eb03b5daad8e2369
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-07 00:00:45 -0500

    Created the DeleteTaskInteractor, the execute method deletes and saves so that the CSV file is empty.

--- src/use_case/delete_task/DeleteTaskInteractor.java
+++ src/use_case/delete_task/DeleteTaskInteractor.java
@@ -1,4 +1,21 @@
 package use_case.delete_task;
 
+import java.util.ArrayList;
+
 public class DeleteTaskInteractor {
+    final DeleteTaskDataAccessInterface deleteDataAccessObject;
+    final DeleteTaskOutputBoundary deletePresenter;
+
+    public DeleteTaskInteractor(DeleteTaskDataAccessInterface deleteDataAccessObject, DeleteTaskOutputBoundary deletePresenter) {
+        this.deleteDataAccessObject = deleteDataAccessObject;
+        this.deletePresenter = deletePresenter;
+    }
+
+    public void execute() {
+        DeleteTaskOutputData deleteTaskOutputData = new DeleteTaskOutputData(false, deleteDataAccessObject.getAllTasksDelete().keySet());
+        deleteDataAccessObject.delete(deleteDataAccessObject.getAllTasksDelete());
+        deleteDataAccessObject.save();
+        deletePresenter.prepareSuccessView(deleteTaskOutputData);
+
+    }
 }
