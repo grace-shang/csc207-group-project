b09da179c0e25eb842e18bd5b038f63d1657b262
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-06 23:56:20 -0500

    Implemented the prepare success view for the DeleteTaskPresenter.

--- src/interface_adapter/delete_task/DeleteTaskPresenter.java
+++ src/interface_adapter/delete_task/DeleteTaskPresenter.java
@@ -1,2 +1,30 @@
-package interface_adapter.delete_task;public class DeleteTaskPresenter {
+package interface_adapter.delete_task;
+
+import interface_adapter.ViewManagerModel;
+import use_case.delete_task.DeleteTaskOutputBoundary;
+import use_case.delete_task.DeleteTaskOutputData;
+
+public class DeleteTaskPresenter implements DeleteTaskOutputBoundary {
+
+    private final DeleteTaskViewModel deleteViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public DeleteTaskPresenter(DeleteTaskViewModel deleteViewModel, ViewManagerModel viewManagerModel) {
+        this.deleteViewModel = deleteViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(DeleteTaskOutputData response) {
+
+        DeleteTaskState deleteTaskState = deleteViewModel.getState();
+        deleteTaskState.setTasks(response.getAllTasks());
+        this.deleteViewModel.setState(deleteTaskState);
+        deleteViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(deleteViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+
 }
