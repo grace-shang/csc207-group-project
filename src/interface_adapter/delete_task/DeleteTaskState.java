98452344cc4aef78c16b2c2b721fd9ddda4beaa6
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-06 23:57:04 -0500

    Implemented the DeleteTaskState including a set of strings of task names.

--- src/interface_adapter/delete_task/DeleteTaskState.java
+++ src/interface_adapter/delete_task/DeleteTaskState.java
@@ -1,2 +1,20 @@
-package interface_adapter.delete_task;public class DeleteTaskState {
+package interface_adapter.delete_task;
+
+import java.util.Set;
+
+public class DeleteTaskState {
+
+    private Set<String> tasks = null;
+
+    public DeleteTaskState(DeleteTaskState copy) {
+        tasks = copy.tasks;
+    }
+
+    public DeleteTaskState() {
+    }
+
+    public Set<String> getTasks() {return tasks;}
+
+    public void setTasks(Set<String> tasks) {this.tasks = tasks;}
+
 }
