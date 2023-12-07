b0c6c743ba2635ff1b553fea7b2b8cffabb60748
Sana-E-Zehra Mehdi <sana.mehdi@mail.utoronto.ca>
2023-12-06 19:37:53 -0500

    Creating the appropriate Java Docs for the Task entity.

--- src/entity/Task.java
+++ src/entity/Task.java
@@ -7,7 +7,11 @@ public class Task implements TaskI{
 
     long taskId;
 
-    // Default constructor. Default value of complete is false.
+    /**
+     * Constrcuts a task with complete variable being always false
+     * @param taskName a string containing the name of the task
+     * @param taskId the ID of the task from the API
+     */
     public Task(String taskName, long taskId) {
         this.taskName = taskName;
         this.complete = false;
@@ -15,27 +19,54 @@ public class Task implements TaskI{
 
     }
 
-    // Overloaded constructor with the optional parameter of isCompleted.
+    /**
+     * Constructs a task with a given complete variable
+     * @param taskName a string containing the name of the task
+     * @param isCompleted a boolean containing whether the task is complete (true) or not (false)
+     * @param taskId the ID of the task from the API
+     */
     public Task(String taskName, boolean isCompleted, long taskId) {
         this.taskName = taskName;
         this.complete = isCompleted;
         this.taskId = taskId;
     }
 
+    /**
+     * A getter for the task name
+     * @return a string containing the task name
+     */
     public String getName() {return taskName;}
 
+    /**
+     * A getter for the completion of a task
+     * @return a boolean containing whether the task is complete or not
+     */
     public boolean getComplete() {return complete;}
 
+    /**
+     * A setter for the task name
+     * @param name a string containing the new name of the task
+     */
     public void setName(String name) {this.taskName = name;}
 
+    /**
+     * A setter for the completion of a task
+     * @param complete a boolean containing the new completion of the task
+     */
     public void setComplete(boolean complete) {this.complete = complete;}
 
-    @Override
+    /**
+     * A setter for the task ID
+     * @param taskId the new task ID to be set
+     */
     public void setTaskId(long taskId) {
         this.taskId = taskId;
     }
 
-    @Override
+    /**
+     * A getter for the task ID
+     * @return a long containing the task ID
+     */
     public long getTaskId() {
         return taskId;
     }
